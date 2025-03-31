d38c8d3334cdf6db7ef67cc1e8eeb7b9ff81e0fb
Kudrin_Demyan <kudrindemyan@gmail.com>
2025-03-31 11:35:09 +0300

    l

--- LeetCode/0073.py
+++ LeetCode/0073.py
@@ -3,11 +3,11 @@ def setZeroes(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j]==0:
-                l.append([i,j])
-    for i in range(len(l)):
+                indexes.append([i,j])
+    for i in range(len(indexes)):
         for j in range(len(matrix)):
-            matrix[j][l[i][1]]=0
+            matrix[j][indexes[i][1]]=0
         for k in range(len(matrix[0])):
-            matrix[l[i][0]][k]=0
+            matrix[indexes[i][0]][k]=0
     print(matrix)
 print(setZeroes([[1,1,0],[1,1,1],[1,1,1]]))
